2b3e19a7c9e083945735d13a10fb4f18c8d68b9d
udith <udithbiju2@gmail.com>
2025-05-26 21:37:42 +0530

    updated serever to restict only one tab for active device

--- backend/server.ts
+++ backend/server.ts
@@ -8,40 +8,51 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
-    },
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"],
+	},
 });
 
 // Store peers with their public IPs
-// const peers :{[socketId: string]: {publicIP:string}} = {};
-const peers: { [socketId: string]: string } = {};
+const peers: { [deviceId: string]: { socketId: string; publicIp: string } } =
+	{};
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
 app.get("/", async (req: Request, res: Response) => {
 	console.log("test");
-    res.status(200).json({ message: "Server is running" });
+	res.json({ message: "Hello from the server!" });
 });
 
-//socket.io connection listening for client which have socket.io 
+//socket.io connection listening for client which have socket.io
 io.on("connection", (socket) => {
-    const publicIp = socket.handshake.headers['x-forwarded-for'] || socket.request.connection.remoteAddress || "unknown";
-    // peers[socket.id] = { publicIP: publicIp as string };
-      peers[socket.id] = publicIp.toString();
-    console.log(`New connection: ${socket.id} from ${publicIp}`);
-    console.log("Current peers:", peers);
+	const publicIp =
+		socket.handshake.headers["x-forwarded-for"] ||
+		socket.request.connection.remoteAddress ||
+		"unknown";
+	const deviceId = socket.handshake.query.deviceId as string;
 
-    socket.on("disconnect", () => {
-        console.log(`Peer disconnected: ${socket.id}`);
-        delete peers[socket.id];
-        console.log("Current peers after disconnect:", peers);
-    });
-})
-app.listen(4001, () => {
+	if (deviceId) {
+		peers[deviceId] = { socketId: socket.id, publicIp: publicIp.toString() };
+		console.log(
+			`New connection: ${socket.id} from ${publicIp} ,device ${deviceId}`
+		);
+		console.log("Current peers:", peers);
+
+		socket.on("disconnect", () => {
+			console.log(`Peer disconnected: ${socket.id}`);
+			delete peers[deviceId];
+			console.log("Current peers after disconnect:", peers);
+		});
+	}else{
+		console.log("Device ID not provided in handshake query");
+		socket.disconnect();
+	}
+});
+
+server.listen(4001, "0.0.0.0", () => {
 	console.log("Server is running on port 4001");
 });
